de4585efc007194a930a0bd392c15c22feda83cf
ckaie <s214750@students.aie.edu.au>
2022-04-11 22:38:57 +1000

    Added 'HelloGit' project.

---
+++ HelloGit/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+/// <summary>
+/// Simple 'HelloGit' program to demonstrate git branching and conflict
+/// resolution
+/// </summary>
+/// <param name="argc"></param>
+/// <param name="argv"></param>
+/// <returns></returns>
+int main(int argc, char* argv[]) {
+	std::cout << "Hello Git!" << std::endl;
+}
